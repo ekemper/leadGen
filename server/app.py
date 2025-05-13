8a7e743c097dad70a8ce691ff5cc62bea979346c
Edward Kemper <edwardkemper@gmail.com>
2025-05-13 03:31:31 -0600

    build step

--- server/app.py
+++ server/app.py
@@ -205,3 +205,4 @@ if __name__ == '__main__':
         app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
     
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), use_reloader=True) 
+    
